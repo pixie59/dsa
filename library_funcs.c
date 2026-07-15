13b383b86b2fb412d24e545986d443ca0d4b6e6d
Hitanshi1106 <24051552@kiit.ac.in>
2026-07-15 21:32:09 +0530

    understood malloc calloc and realloc

---
+++ library_funcs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    // int *a=(int *)malloc(n*sizeof(int));
+    int *a=(int *)calloc(n,sizeof(int));
+    for(int i=0;i<n;i++){
+        a[i]=i+1;
+        printf("%d ",a[i]);
+    }
+}
