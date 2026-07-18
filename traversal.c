fc4efb26f1b93f6c2599170d1eacda093491d563
Hitanshi1106 <24051552@kiit.ac.in>
2026-07-18 21:35:40 +0530

    Revised traversal

---
+++ traversal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    int *a=(int *)malloc(n*sizeof(int));
+    for(int i=0;i<n;i++){
+        a[i]=i+1;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    free(a);
+    return 0;
+}
